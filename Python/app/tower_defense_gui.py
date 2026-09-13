c605250e692fc2c3f050ccae4b70dbdca741b2f5
WinoSpop <bloxy20200@gmail.com>
2026-09-13 18:10:03 -0500

    Correcion de bug del import path for tower_defense_gui.py

--- Python/app/tower_defense_gui.py
+++ Python/app/tower_defense_gui.py
@@ -9,6 +9,8 @@ Ejecutar con:  python3 tower_defense_gui.py
 """
 
 import tkinter as tk
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from tkinter import ttk, messagebox
 from tkinter import ttk, messagebox
 
 from modelo.torre import Torre
